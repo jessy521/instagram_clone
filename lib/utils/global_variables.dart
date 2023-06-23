50cdf0b8227e868b35e54070b297131faefbaf84
jessy521 <jessykumarsarkar@gmail.com>
2023-06-23 21:43:08 +0530

    feed_post added

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
-  const Text('home'),
+  const FeedScreen(),
   const Text('search'),
   const AddPostScreen(),
   const Text('notify'),
